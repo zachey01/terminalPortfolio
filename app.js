fe8c1477bb81dde66240a93383c47bcf2cbf5999
Zachey <fradexo56@gmail.com>
2023-07-03 21:08:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -74,14 +74,11 @@ function onError(error) {
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
-
   if (isNaN(port)) {
     return val;
   }
-
   if (port >= 0) {
     return port;
   }
-
   return false;
 }
