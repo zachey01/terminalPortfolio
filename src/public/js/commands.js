652fb1d1875ce52262eddf775d8d5329cb77e255
zachey <63107653+zachey01@users.noreply.github.com>
2023-07-04 13:38:45 +0300

    update commands

--- src/public/js/commands.js
+++ src/public/js/commands.js
@@ -1,159 +1,72 @@
-var suggestions = [
-  'help',
-  'skills',
-  'clear',
-  'projects',
-  'blog',
-  'tools',
-  /* Links */
-  'github',
-  'telegram',
-  'discord',
-  'email',
-  'steam',
-  'youtube',
-];
-
 window.addEventListener('DOMContentLoaded', function () {
-  var cmdInput = document.getElementById('cmd');
+  // Get references to HTML elements
+  let cmdInput = document.getElementById('cmd');
   cmdInput.focus();
-  var outputDiv = document.getElementById('output');
-  var mainInfoDiv = document.getElementById('mainInfo');
-  var terminalDiv = document.getElementById('terminal');
+  let helpCmdListDiv = document.getElementById('helpCmdList');
+  helpCmdListDiv.innerHTML = helpCmd;
+  let outputDiv = document.getElementById('output');
+  let mainInfoDiv = document.getElementById('mainInfo');
+  let terminalDiv = document.getElementById('terminal');
 
   cmdInput.addEventListener('keypress', function (e) {
     if (e.keyCode === 13) {
-      var cmd = cmdInput.value.trim();
+      // Check if Enter key is pressed
+      let cmd = cmdInput.value.trim(); // Get the command entered by the user and remove leading/trailing whitespace
+
       if (cmd === '') {
-        return;
+        return; // If the command is empty, do nothing
       }
 
       outputDiv.innerHTML +=
         "<div><span class='ownerTerminal'><b>zachey@profile</b></span>:<b>~$</b> " +
         cmd +
-        '</div>';
+        '</div>'; // Display the entered command in the output div
 
-      cmdInput.value = '';
+      cmdInput.value = ''; // Clear the command input field
+
+      // Check the entered command and take appropriate actions
 
       if (cmd === 'skills' || cmd === 's') {
-        outputDiv.innerHTML += `
-  <div class="container">
-    <div class="flex">
-      <h2>HTML/EJS:</h2>
-      <div class="skillBar">
-        <div class="skillBarItem1"></div>
-      </div>
-      <h3>100%</h3>
-    </div>
-  
-    <div class="flex">
-      <h2>CSS/SCSS:</h2>
-      <div class="skillBar">
-        <div class="skillBarItem2"></div>
-      </div>
-      <h3>100%</h3>
-    </div>
-  
-    <div class="flex">
-      <h2>JS:</h2>
-      <div class="skillBar">
-        <div class="skillBarItem3"></div>
-      </div>
-      <h3>95%</h3>
-    </div>
-  
-    <div class="flex">
-      <h2>TS:</h2>
-      <div class="skillBar">
-        <div class="skillBarItem4"></div>
-      </div>
-      <h3>55%</h3>
-    </div>
-  
-    <div class="flex">
-      <h2>NODE.JS:</h2>
-      <div class="skillBar">
-        <div class="skillBarItem5"></div>
-      </div>
-      <h3>85%</h3>
-    </div>
-  
-    <div class="flex">
-      <h2>REACT.JS:</h2>
-      <div class="skillBar">
-        <div class="skillBarItem6"></div>
-      </div>
-      <h3>15%</h3>
-    </div>
-  
-    <div class="flex">
-      <h2>GO:</h2>
-      <div class="skillBar">
-        <div class="skillBarItem7"></div>
-      </div>
-      <h3>5%</h3>
-    </div>
-  
-    <div class="flex">
-    <h2>RUST:</h2>
-    <div class="skillBar">
-      <div class="skillBarItem8"></div>
-    </div>
-    <h3>5%</h3>
-  </div>
-  </div>`;
+        // Display the skills information in the output div
+        outputDiv.innerHTML += skillsBar;
       }
       // Links
       else if (cmd === 'github' || cmd === 'gh') {
-        window.location.href = 'https://github.com/zachey01';
+        window.location.href = 'https://github.com/zachey01'; // Redirect to GitHub profile
       } else if (cmd === 'discord' || cmd === 'ds') {
-        window.location.href = 'https://discord.com/users/1033246411363471472';
+        window.location.href = 'https://discord.com/users/1033246411363471472'; // Redirect to Discord profile
       } else if (cmd === 'telegram' || cmd === 'tg') {
-        window.location.href = 'https://t.me/ImZachey';
+        window.location.href = 'https://t.me/ImZachey'; // Redirect to Telegram profile
       } else if (cmd === 'email' || cmd === 'em') {
-        window.location.href = 'mailto:zachey@bk.ru';
+        window.location.href = 'mailto:zachey@bk.ru'; // Open default email client with pre-filled email address
       } else if (cmd === 'steam' || cmd === 'st') {
-        window.location.href = 'https://steamcommunity.com/id/zachey01';
-      } else if (cmd === 'spotify' || cmd === 'sp') {
-        window.location.href = 'https://steamcommunity.com/id/zachey01';
+        window.location.href = 'https://steamcommunity.com/id/zachey01'; // Redirect to Steam profile
       } else if (cmd === 'youtube' || cmd === 'yt') {
-        window.location.href = 'https://www.youtube.com/@zachey01';
+        window.location.href = 'https://www.youtube.com/@zachey01'; // Redirect to YouTube channel
       }
 
       // Pages
       else if (cmd === 'projects' || cmd === 'pj') {
-        window.location.href = '/projects';
+        window.location.href = '/projects'; // Redirect to projects page
       } else if (cmd === 'blog') {
-        window.location.href = '/blog';
+        window.location.href = '/blog'; // Redirect to blog page
       }
 
       // Other
       else if (cmd === 'help') {
-        outputDiv.innerHTML += `<br>Available commands: <br />[<span class="commandName">skills</span>] or
-        [<span class="commandName">s</span>] <br />[<span class="commandName"
-          >projects</span
-        >] or [<span class="commandName">pj</span>] <br /><br />[<span
-          class="commandName"
-          >clear</span
-        >] <br /><br />Contact me: <br />[<span class="commandName">github</span>]
-        <br />[<span class="commandName">discord</span>] <br />[<span
-          class="commandName"
-          >telegram</span
-        >] <br />[<span class="commandName">email</span>] <br />[<span
-          class="commandName"
-          >steam</span
-        >] <br />[<span class="commandName">youtube</span>]`;
+        // Display available commands and contact information
+        outputDiv.innerHTML += helpCmd;
       }
 
       // Clear terminal
       else if (cmd === 'clear' || cmd === 'c') {
-        outputDiv.innerHTML = ''; // Удаление содержимого при вводе команды "clear"
-        mainInfoDiv.innerHTML = '';
+        outputDiv.innerHTML = ''; // Clear the output div
+        mainInfoDiv.innerHTML = ''; // Clear the main info div
       } else {
-        outputDiv.innerHTML += '<div>Not found</div>';
+        outputDiv.innerHTML += '<div>Not found</div>'; // Display "Not found" message if the command is not recognized
       }
 
-      outputDiv.scrollTop = outputDiv.scrollHeight;
+      outputDiv.scrollTop = outputDiv.scrollHeight; // Scroll to the bottom of the output div
     }
   });
 });
---
+++ src/public/js/vars.js
@@ -0,0 +1,104 @@
+let suggestions = [
+  'help',
+  'skills',
+  'clear',
+  'projects',
+  'blog',
+  'tools',
+  // Links
+  'github',
+  'telegram',
+  'discord',
+  'email',
+  'steam',
+  'youtube',
+];
+
+// Commands
+let helpCmd = `
+  <br>Available commands: <br />
+  [<span class="commandName">skills</span>] or [<span class="commandName">s</span>]
+  <br />
+  [<span class="commandName">projects</span>] or [<span class="commandName">pj</span>]
+  <br /><br />
+  [<span class="commandName">clear</span>]
+  <br /><br />
+  Contact me: <br />
+  [<span class="commandName">github</span>]
+  <br />
+  [<span class="commandName">discord</span>]
+  <br />
+  [<span class="commandName">telegram</span>]
+  <br />
+  [<span class="commandName">email</span>]
+  <br />
+  [<span class="commandName">steam</span>]
+  <br />
+  [<span class="commandName">youtube</span>]`;
+
+let skillsBar = `
+<div class="container">
+  <div class="flex">
+    <h2>HTML/EJS:</h2>
+    <div class="skillBar">
+      <div class="skillBarItem1"></div>
+    </div>
+    <h3>100%</h3>
+  </div>
+
+  <div class="flex">
+    <h2>CSS/SCSS:</h2>
+    <div class="skillBar">
+      <div class="skillBarItem2"></div>
+    </div>
+    <h3>100%</h3>
+  </div>
+
+  <div class="flex">
+    <h2>JS:</h2>
+    <div class="skillBar">
+      <div class="skillBarItem3"></div>
+    </div>
+    <h3>95%</h3>
+  </div>
+
+  <div class="flex">
+    <h2>TS:</h2>
+    <div class="skillBar">
+      <div class="skillBarItem4"></div>
+    </div>
+    <h3>55%</h3>
+  </div>
+
+  <div class="flex">
+    <h2>NODE.JS:</h2>
+    <div class="skillBar">
+      <div class="skillBarItem5"></div>
+    </div>
+    <h3>85%</h3>
+  </div>
+
+  <div class="flex">
+    <h2>REACT.JS:</h2>
+    <div class="skillBar">
+      <div class="skillBarItem6"></div>
+    </div>
+    <h3>15%</h3>
+  </div>
+
+  <div class="flex">
+    <h2>GO:</h2>
+    <div class="skillBar">
+      <div class="skillBarItem7"></div>
+    </div>
+    <h3>5%</h3>
+  </div>
+
+  <div class="flex">
+  <h2>RUST:</h2>
+  <div class="skillBar">
+    <div class="skillBarItem8"></div>
+  </div>
+  <h3>5%</h3>
+</div>
+</div>`;
