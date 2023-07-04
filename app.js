ebd928cc8c336c616499a41be5c8c98144db3891
zachey <63107653+zachey01@users.noreply.github.com>
2023-07-04 14:03:20 +0300

    move "projects" page

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import logger from 'morgan';
 import debug from 'debug';
 import http from 'http';
 import indexRouter from './src/routes/index.js';
-import projectsRouter from './src/routes/projects.js';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -32,7 +31,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './src/public')));
 
 app.use('/', indexRouter);
-app.use('/projects', projectsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- src/routes/projects.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.render('projects', { title: 'Zachey`s projects' });
-});
-
-export default router;
